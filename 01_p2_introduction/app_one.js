615456dfd2c9bbe6d315bdf53680a202d7f701fd
Alain Guillon <zyrass@outlook.fr>
2019-04-14 11:19:43 +0200

    add a v2 introduction

---
+++ 01_p2_introduction/app_one.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* HEAD_CONTENT */
+new Vue({
+    el: "#head_content",
+    data: {
+        head_title: "Learning VueJS - 01 bis"
+    }
+});
+
+/* HEADER_CONTENT */
+new Vue({
+    el: "#header_content",
+    data: {
+        header_title: "01 - Introduction amélioré"
+    }
+});
+
+/* APP_ONE */
+new Vue({
+    el: "#app_one",
+    data: {
+        information: "Your content here..."
+    }
+});
