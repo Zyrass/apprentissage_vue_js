54880e8d4bcc43fda7d19870cc7c64b4a4a49982
Alain Guillon <zyrass@outlook.fr>
2019-04-16 16:12:02 +0200

    i transform my task in object

---
+++ 07_exo_task/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning VueJS - 07 - Continuation of the task manager</title>
+
+    <!-- development version, includes helpful console warnings -->
+    <!-- script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script -->
+    <!-- production version, optimized for size and speed -->
+    <!-- script src="https://cdn.jsdelivr.net/npm/vue"></script -->
+
+    <!-- SCRIPT_VUE_JS -->
+    <script src="../assets/js/vue.js"></script>
+</head>
+<body>
+    <header id="header_content">
+        <h1>{{ header_title }}</h1>
+    </header>
+    
+    <!-- APP_FIVE -->
+    <main id="app_five">
+        <p>
+            {{ info }}
+        </p>
+        <div>
+            <form v-on:submit.prevent="addTask">
+                <input type="text" v-model="task">
+                <button>click here for add your new task!</button>
+            </form>
+            <ul>
+                <li v-for="task in tasks">
+                    <ul>
+                        <li>{{ task.id }}</li>
+                        <li>{{ task.date }}</li>
+                        <li>{{ task.description }}</li>
+                        <li>{{ task.status }}</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </main> <!-- /APP_FIVE -->
+
+    <footer>
+        Footer
+    </footer>
+
+    <!-- SCRIPT_APP_FIVE -->
+    <script>
+
+        /* HEADER_CONTENT */
+        new Vue({
+            el: "#header_content",
+            data: {
+                header_title: "07 - Continuation of the task manager",
+            }
+        });
+
+        /* APP_FIVE */
+        new Vue(
+        {
+            el: "#app_five",
+            data: {
+                info: "I convert the tasks manager in a of object model for our task. And i has add a dynamic id.",
+                task: null,
+                id: 1,
+                tasks: [],
+            }, 
+            methods: {
+                addTask: function() {
+
+                    // push task in tasks table
+                    this.tasks.push({ 
+                        id: this.id++, 
+                        description: this.task, 
+                        date: new Date(), 
+                        status: false 
+                    });
+                    
+                    this.task = null;
+                    // control task in our console
+                    console.table(tasks);
+
+                }
+            }
+        });
+    </script>
+</body>
+</html>
